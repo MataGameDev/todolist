6872ad5791ea16dd6a5f25664f0773a4ecdb038f
MataGitUniat <mikes0174@hotmail.com>
2022-05-02 21:42:00 -0500

    Web Intercambios

---
+++ Docs/precache-manifest.5600ff0bbaab40ce9a38b17f3a4f7966.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "df425214387cb4c809ff",
+    "url": "/static/css/main.c018ca5b.chunk.css"
+  },
+  {
+    "revision": "df425214387cb4c809ff",
+    "url": "/static/js/main.df425214.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "4933064c23f62bd41ef2",
+    "url": "/static/js/2.4933064c.chunk.js"
+  },
+  {
+    "revision": "8ba3075663cac21e8fc5",
+    "url": "/static/js/runtime~main.8ba30756.js"
+  },
+  {
+    "revision": "b70ea44106e1fa471f05fcf4dd496846",
+    "url": "/index.html"
+  }
+];
--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,15 @@
 
 import React from 'react';
-import Header from '../shared/layout/Header';
-import './App.css';
 import Footer from '../shared/layout/Footer';
 import Content from '../shared/layout/Content';
 /*import Todo from './Todo/Todo';*/
 //import Timer from './pomodoro/Timer';
-import Intercambio from './Criptocosas/intercambio';
+import Intercambio from './Criptocosas/Intercambio';
 
 
 function App() {
   return (
     <div className="App">
-      <Header title="Header" url="https://github.com/"/>
       <Content >
         <Intercambio/>
       </Content>
